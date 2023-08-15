ea3dfa0696c72a09b0faef2a7f1badb299f7ab50
Mark <ajineer@outlook.com>
2023-08-15 15:24:30 -0600

    completed lab

--- server/models.py
+++ server/models.py
@@ -12,6 +12,18 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name or not isinstance(name, str):
+            raise ValueError("Name must be non-empty string!")
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if not phone_number or not isinstance(phone_number, str) or len(phone_number) != 10:
+            raise ValueError("Phone number must be non-empty string of 10 digits!")
+        return phone_number
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -27,6 +39,29 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title or not "Won't Believe" in title or not "Secret" in title or not "Top" in title or not "Guess" in title:
+            raise ValueError
+        return title
+    
+    @validates('content')
+    def validate_content(self, key, content):
+        if not content or not isinstance(content, str) or len(content) < 250:
+            raise ValueError
+        return content
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if not summary or not isinstance(summary, str) or len(summary) >= 250:
+            raise ValueError
+        return summary
+
+    @validates('category')
+    def validate_category(self, key, category):
+        if category != 'Fiction' and category != 'Non-Fiction':
+            raise ValueError
+        return category
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
